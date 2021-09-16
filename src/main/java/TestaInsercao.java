a9747adc7bcd51bc6af020ce45b43c5d26c94e66
Francisco Sousa <franciscoeds1987@gmail.com>
2021-09-16 08:09:43 -0300

    Inserindo com Statement

---
+++ src/main/java/TestaInsercao.java
@@ -0,0 +1,20 @@
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaInsercao {
+
+    public static void main(String[] args) throws SQLException {
+        var factory = new ConnectionFactory();
+        var connection = factory.recuperarConexao();
+
+        var statement = connection.createStatement();
+        statement.execute("INSERT INTO PRODUTO (nome, descricao) VALUES ('MOUSE', 'MOUSE SEM FIO')",
+                Statement.RETURN_GENERATED_KEYS);
+
+        var generatedKeys = statement.getGeneratedKeys();
+        while (generatedKeys.next()) {
+            var id = generatedKeys.getInt(1);
+            System.out.println("O id criado foi: " + id);
+        }
+    }
+}
