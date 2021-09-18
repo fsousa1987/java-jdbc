4ffba20ae8c8be5065e2dae1d9f0bea5785e8148
Francisco Sousa <franciscoeds1987@gmail.com>
2021-09-18 07:23:57 -0300

    Lidando com commit e rollback

--- src/main/java/TestaInsercaoComParametro.java
+++ src/main/java/TestaInsercaoComParametro.java
@@ -9,21 +9,31 @@ public class TestaInsercaoComParametro {
         var connection = factory.recuperarConexao();
         connection.setAutoCommit(false);
 
-        var statement = connection
-                .prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
+        try {
+            var statement = connection
+                    .prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
 
-        adicionarVariavel("SMARTTV", "45 POLEGADAS", statement);
-        adicionarVariavel("RADIO", "RADIO DE BATERIA", statement);
+            adicionarVariavel("SMARTTV", "45 POLEGADAS", statement);
+            adicionarVariavel("RADIO", "RADIO DE BATERIA", statement);
+
+            connection.commit();
+            statement.close();
+            connection.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("ROLLBACK EXECUTADO");
+            connection.rollback();
+        }
     }
 
     private static void adicionarVariavel(String nome, String descricao, PreparedStatement statement) throws SQLException {
         statement.setString(1, nome);
         statement.setString(2, descricao);
-        /*
+
         if (nome.equals("RADIO")) {
             throw new RuntimeException("Não foi possível adicionar o produto");
         }
-         */
 
         statement.execute();
 
