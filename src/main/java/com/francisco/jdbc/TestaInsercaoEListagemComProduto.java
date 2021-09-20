2e8581d5e288ba5a9a354cfadf827016d6d07f33
Francisco Sousa <franciscoeds1987@gmail.com>
2021-09-20 09:41:13 -0300

    Listando no ProdutoDAO

--- src/main/java/com/francisco/jdbc/TestaInsercaoEListagemComProduto.java
+++ src/main/java/com/francisco/jdbc/TestaInsercaoEListagemComProduto.java
@@ -5,7 +5,7 @@ import com.francisco.jdbc.modelo.Produto;
 
 import java.sql.*;
 
-public class TestaInsercaoComProduto {
+public class TestaInsercaoEListagemComProduto {
 
     public static void main(String[] args) throws SQLException {
         var comoda = new Produto("Cômoda", "Cômoda vertical");
@@ -13,8 +13,8 @@ public class TestaInsercaoComProduto {
         try(var connection = new ConnectionFactory().recuperarConexao()) {
             var produtoDAO = new ProdutoDAO(connection);
             produtoDAO.salvar(comoda);
-
+            var listaDeProdutos = produtoDAO.listar();
+            listaDeProdutos.forEach(System.out::println);
         }
-        System.out.println(comoda);
     }
 }
--- src/main/java/com/francisco/jdbc/dao/ProdutoDAO.java
+++ src/main/java/com/francisco/jdbc/dao/ProdutoDAO.java
@@ -2,9 +2,9 @@ package com.francisco.jdbc.dao;
 
 import com.francisco.jdbc.modelo.Produto;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProdutoDAO {
 
@@ -29,5 +29,25 @@ public class ProdutoDAO {
                 }
             }
         }
+    }
+
+    public List<Produto> listar() throws SQLException {
+        var produtos = new ArrayList<Produto>();
+
+        var sql = "SELECT ID, NOME, DESCRICAO FROM PRODUTO";
+
+        try(var preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.execute();
+
+            try(var resultSet = preparedStatement.getResultSet()) {
+                while (resultSet.next()) {
+                    Produto produto =
+                            new Produto(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3));
+
+                    produtos.add(produto);
+                }
+            }
+        }
+        return produtos;
     }
 }
